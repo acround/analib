a667ae7a840a2ebb7955cc58c96b67c236798274
Acround <acround@gmail.com>
2025-07-09 14:08:47 +0200

    .

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once ANALIB_DIR_LIB . DIRECTORY_SEPARATOR . 'constants.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'constants.php';
 require_once ANALIB_DIR_LIB_EXCEPTIONS . DIRECTORY_SEPARATOR . 'BaseException' . ANALIB_EXT_EXCEPTION;
 require_once ANALIB_DIR_LIB_EXCEPTIONS . DIRECTORY_SEPARATOR . 'NotFoundException' . ANALIB_EXT_EXCEPTION;
 require_once ANALIB_DIR_LIB_EXCEPTIONS . DIRECTORY_SEPARATOR . 'ClassNotFoundException' . ANALIB_EXT_EXCEPTION;
